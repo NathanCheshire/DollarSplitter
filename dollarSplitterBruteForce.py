08d635c0e129f87c886522596e7eab417e94d75c
Nathan <nathan.vincent.2.718@gmail.com>
2021-08-26 11:55:01 -0500

    add timing

--- dollarSplitterBruteForce.py
+++ dollarSplitterBruteForce.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 PENNY = 1
 NICKEL = 5
@@ -6,7 +7,7 @@ DIME = 10
 QUARTER = 25;
 
 #ONLY CHANGE THE NUMBER HERE
-dollarAmount = 2
+dollarAmount = 1
 
 if (math.floor(dollarAmount) == dollarAmount):
     print('Provided dollar amount: ', dollarAmount)
@@ -18,6 +19,8 @@ else:
 ways = 0
 cents = dollarAmount * 100
 
+start = time.time() * 1000
+
 for numQuarters in range(int(cents / QUARTER), -1, -1):
     for numDimes in range(int(cents / DIME), -1, -1):
         for numNickels in range(int(cents / NICKEL), -1, -1):
@@ -25,4 +28,6 @@ for numQuarters in range(int(cents / QUARTER), -1, -1):
                 if (numQuarters * QUARTER + numDimes * DIME + numNickels * NICKEL + numPennies * PENNY == cents):
                     ways += 1
 
-print(ways)
+totalTime = time.time() * 1000 - start
+
+print(ways," found in ",round(totalTime),"ms")
